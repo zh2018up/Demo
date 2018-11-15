54fc078e448d357eeac09ecbeda5fadd4433bc81
jack <jack@xx.com>
2018-11-15 11:57:53 +0800

    version_1.0

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Auther Administrator
+ * @Date 2018-11-15 11:56
+ * @Version 1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
