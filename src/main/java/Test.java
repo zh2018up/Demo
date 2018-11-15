30083b180db7f70f08a2e3c92b594a6af4e18561
jack <jack@xx.com>
2018-11-15 12:01:39 +0800

    version_2.0

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,4 +9,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public void print () {
+        System.out.println("Hello World!");
+    }
 }
